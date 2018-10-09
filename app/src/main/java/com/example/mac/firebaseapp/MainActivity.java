4b2e304b0aa4868082512622dd2794401f25e672
Maciej Ćmiel <maciej.cmiel.S@ee.pw.edu.pl>
2018-10-09 22:19:59 +0200

    Add ChildEventListener

--- app/src/main/java/com/example/mac/firebaseapp/MainActivity.java
+++ app/src/main/java/com/example/mac/firebaseapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mac.firebaseapp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.InputFilter;
@@ -14,6 +16,9 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference messagesDatabaseReference;
+    private ChildEventListener childEventListener;
 
     private static final String TAG = "MainActivity";
 
@@ -85,6 +91,41 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                //getValue() can take parameter which is a class
+                //by passing in class as a parameter code will deserialize the message
+                //from the DB into our FM object
+                mMessageAdapter.add(dataSnapshot.getValue(FriendlyMessage.class));
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                //when child message change position in the list
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //when some error occurred when trying to make changes
+                //typically you don't have permission to read the data
+
+            }
+        };
+        //only triggered when one of children of the "messages" node changes
+        messagesDatabaseReference.addChildEventListener(childEventListener);
 
         // Enable Send button when there's text to send
         mMessageEditText.addTextChangedListener(new TextWatcher() {
